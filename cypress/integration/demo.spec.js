e56fd0b7d0a006c5d8478fd805ca433601cd8f22
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-01-10 07:54:57 -0600

    updated cypress/integration/demo.spec.js

--- cypress/integration/demo.spec.js
+++ cypress/integration/demo.spec.js
@@ -1,17 +1,23 @@
 /* global context: false, cy: false */
 import 'cypress-axe';
-import axe from 'axe-core';
+//import axe from 'axe-core';
 
 const URL = 'http://localhost:3000/';
 
-axe.configure({
-  rules: [{'page-has-heading-one': {enabled: false}}]
-});
+//axe.configure([
+//  rules: [{'page-has-heading-one': {enabled: false}}]
 
 context('basic', () => {
   beforeEach(() => {
-    cy.visit(URL); // must preceded injectAxe call
+    cy.visit(URL); // must precede injectAxe call
     cy.injectAxe();
+    /*
+    cy.configureAxe({
+      rules: [{
+        'page-has-heading-one': {enabled: false}
+      }]
+    });
+    */
   });
 
   it('home page content', () => {
